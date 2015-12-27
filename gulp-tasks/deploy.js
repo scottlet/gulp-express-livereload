fad7c13c69b89676b008bc89da76efc6b139a669
Scott van Looy <scott@quul.net>
2015-12-27 16:40:53 +0100

    Update deploy.js
    Removed extra code

--- gulp-tasks/deploy.js
+++ gulp-tasks/deploy.js
@@ -6,10 +6,6 @@ function copyDeploy () {
     return gulp.src([CONSTS.APPSERVER_DEST + '/**/*', CONSTS.APP_PATH + '/**/*', 'package.json', 'app.js'], {base: '.'})
     .pipe(gulp.dest(CONSTS.DEPLOY_DEST));
 }
-function npmInstall () {
-    return gulp.src(CONSTS.DEPLOY_DEST);
-}
 
 gulp.task('copydeploy', ['build'], copyDeploy);
-gulp.task('npm-install', ['copydeploy'], npmInstall);
-gulp.task('deploy', ['copydeploy', 'npm-install']);
+gulp.task('deploy', ['copydeploy']);
