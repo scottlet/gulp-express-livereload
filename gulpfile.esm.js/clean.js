4ae19005fb9a0282319c232dc9b3b701514b9fe2
Scott van Looy <scott@quul.net>
2023-02-28 11:16:25 +0100

    Updated dependencies

--- gulpfile.esm.js/clean.js
+++ gulpfile.esm.js/clean.js
@@ -1,10 +1,10 @@
-import del from 'del';
+import { deleteAsync } from 'del';
 
 import { CONSTS } from './CONSTS';
 const { APPSERVER_DEST, DEPLOY_DEST } = CONSTS;
 
 function clean() {
-    return del([APPSERVER_DEST, DEPLOY_DEST]);
+    return deleteAsync([APPSERVER_DEST, DEPLOY_DEST]);
 }
 
 export { clean };
